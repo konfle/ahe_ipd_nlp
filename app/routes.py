57417f6ab58a77a6f90a9f59a535cf9c9682fb06
konfle <konrad.flejter@gmail.com>
2024-05-12 18:47:57 +0200

    Add classifier selection and 404 view.

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, request, Blueprint
 from nb_classifier import NaiveBayesClassifier
 
 # Load the spam classifier
-loaded_classifier, train_metrics = NaiveBayesClassifier.load_classifier("../models/naive_bayes_classifier.pkl",
-                                                                        "../data/spam_NLP.csv")
+naive_bayes_classifier, train_metrics = NaiveBayesClassifier.load_classifier("../models/naive_bayes_classifier.pkl",
+                                                                             "../data/spam_NLP.csv")
 # Create a blueprint for routes
 bp = Blueprint("routes", __name__)
 
@@ -19,19 +19,27 @@ def index():
     return render_template('index.html')
 
 
-@bp.route('/classify', methods=['POST'])
+@bp.route("/classify", methods=["POST"])
 def classify_email():
     """
-    Classify an email as spam or not spam.
+    Classify an email as spam or not spam using the selected classifier.
 
     Returns:
         str: Rendered HTML content for the result page.
     """
-    if request.method == 'POST':
-        email_text = request.form['email_text']
-        prediction = loaded_classifier.predict(email_text)
-        if prediction == 1:
-            result = 'Spam'
+    if request.method == "POST":
+        email_text = request.form["email_text"]
+        selected_classifier = request.form["classifier"]
+
+        if selected_classifier == "naive_bayes":
+            prediction = naive_bayes_classifier.predict(email_text)
+        elif selected_classifier == "not_implemented_yet":
+            return render_template("404.html")
         else:
-            result = 'Not Spam'
-        return render_template('result.html', result=result, metrics=train_metrics)
+            return render_template("404.html")
+
+        if prediction == 1:
+            result = "Spam"
+        else:
+            result = "Not Spam"
+        return render_template("result.html", result=result, metrics=train_metrics)
